338dd086d82407d8273c8488764caaf22075cba5
ReyesRamirez <a.22308051281289@cbtis128.edu.mx>
2024-09-09 10:12:35 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,17 @@
+print("Ejemplo de listas")
+arcoiris=["verde","azul","morado"]
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elementos que contiene la lista arcoiris ", longitud)
+print(f"Elementos que contiene la lista arcoiris v2 {longitud}")
+print("Accediendo a un elemento de la lista")
+print(arcoiris[1])
+print(f"Elemento en la posision 0 es {arcoiris[0]}")
+print(f"Elemento en la posision 0 es: ",arcoiris[0])
+print("Agregar a un elemento de la lista")
+print(arcoiris)
+arcoiris.append("naranja")
+print(arcoiris)
+print("Listar lo elementos de la lista ciclo for")
+for Ramirez in arcoiris:
+    print(Ramirez)
